313f7f0decea4f3f15e2e3f731f92bab720b9251
Aleksandr2905 <natalukha@ukr.net>
2023-09-07 18:53:18 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+let intervalId;
+
 const refs = {
     startBtnEl: document.querySelector('[data-start]'),
     stopBtnEl: document.querySelector('[data-stop]'),
